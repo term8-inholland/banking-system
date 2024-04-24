e2d2752899016154724d44eabc23fc36dad47a2d
piguycs <kunal.a.dandekar@gmail.com>
2024-04-24 19:29:17 +0200

    removed jooq :( in favour of jpa + spring-data

--- backend/src/main/java/me/piguy/inholland/sisyphus/configuration/BeanFactory.java
+++ backend/src/main/java/me/piguy/inholland/sisyphus/configuration/BeanFactory.java
@@ -1,32 +1,13 @@
 package me.piguy.inholland.sisyphus.configuration;
 
 import me.piguy.inholland.sisyphus.service.IbanService;
-import org.jooq.DSLContext;
-import org.jooq.impl.DSL;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-
 @Configuration
 public class BeanFactory {
-
-    private final DataSource dataSource;
-
-    @Autowired
-    public BeanFactory(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
+    @Bean
     public IbanService ibanService() {
         return new IbanService();
-    }
-
-    @Bean
-    public DSLContext ctx() throws Exception {
-        Connection connection = dataSource.getConnection();
-        return DSL.using(connection);
     }
 }
--- backend/src/main/java/me/piguy/inholland/sisyphus/model/User.java
+++
@@ -1,6 +0,0 @@
-package me.piguy.inholland.sisyphus.model;
-
-import java.util.UUID;
-
-public record User(UUID id, String first_name, String last_name, String password) {
-}
